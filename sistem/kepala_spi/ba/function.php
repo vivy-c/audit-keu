736eb178ced0901b087114a2d09442b8b85a0483
Vivy Cahyani <cahyanivivy@gmail.com>
2021-11-15 10:19:49 +0700

    membuat file function pada folder berita acara

---
+++ sistem/kepala_spi/ba/function.php
@@ -0,0 +1,80 @@
+<?php
+
+$tb_tha = query("SELECT * FROM tb_tha ORDER BY tgl_tha ASC");
+
+if (isset($_POST["tambahLHA"])) {
+    //cek data berhasil tambah atau tidak
+    if (tambahLHA($_POST) > 0) {
+        echo "
+              <script>
+              alert('data berhasil ditambahkan');
+              document.location.href='index.php';
+              </script>
+              ";
+    } else {
+        echo "
+              <script>
+              alert('data gagal ditambahkan');
+              document.location.href='index.php';
+              </script>
+              ";
+    }
+}
+if (isset($_POST["ubahLHA"])) {
+    //cek data berhasil tambah atau tidak
+    if (ubahLHA($_POST) > 0) {
+        echo "
+              <script>
+              alert('data berhasil diubah');
+              document.location.href='index.php';
+              </script>
+              ";
+    } else {
+        echo "
+              <script>
+              alert('data gagal diubah');
+              document.location.href='index.php';
+              </script>
+              ";
+    }
+}
+
+function tambahLHA($data)
+{
+    global $conn;
+    $id_lha          = htmlspecialchars($data["id_lha"]);
+    $id_tha        = htmlspecialchars($data["id_tha"]);
+    $tgl_lha       = htmlspecialchars($data["tgl_lha"]);
+    $status         = htmlspecialchars($data["status"]);
+    $keterangan     = htmlspecialchars($data["keterangan"]);
+    //insert data
+    $query = "INSERT INTO tb_lha VALUES 
+    ('$id_lha','$id_tha','$tgl_lha','$status','$keterangan')";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+function ubahLHA($data)
+{
+    global $conn;
+    $id_lha          = htmlspecialchars($data["id_lha"]);
+    $id_tha        = htmlspecialchars($data["id_tha"]);
+    $tgl_lha       = htmlspecialchars($data["tgl_lha"]);
+    $status         = htmlspecialchars($data["status"]);
+    $keterangan     = htmlspecialchars($data["keterangan"]);
+    //update data
+    $query="UPDATE tb_lha SET 
+
+      id_tha     = '$id_tha',
+      tgl_lha      = '$tgl_lha',
+      status      = '$status',
+      keterangan  = '$keterangan'
+
+      WHERE id_lha = '$id_lha'
+      ";
+      mysqli_query($conn,$query);
+      return mysqli_affected_rows($conn);
+}
+
+
+?>
--- sistem/kepala_spi/ba/index.php
+++ sistem/kepala_spi/ba/index.php
@@ -1,29 +1,10 @@
 <?php
 include('../../template/header.php');
 include('../../template/sidebar_kepala_spi.php');
-
+include('function.php');
 
 $username = $_SESSION["username"];
 
-// $tampil = query("SELECT * FROM tb_user WHERE status = 0 ");
-$tb_ba = query("SELECT * FROM tb_ba");
-// $id_user=$_GET["id_user"];
-// $q=query("SELECT * FROM tb_user WHERE id_user = $id_user")[0];
-
-
-if (isset($_POST["status"])) {
-  if (status($_POST) > 0) {
-     echo "<script>
-     alert('User berhasil diaktifkan');
-     document.location.href='../user/index.php';
-     </script>";
-  } else {
-     echo "<script>
-     alert('User gagal diaktifkan');
-     document.location.href='../user/index.php';
-     </script>";
-  }
-}
 ?>
 
 
@@ -38,6 +19,11 @@ if (isset($_POST["status"])) {
       <!-- Left col -->
         <div class="container-fluid">
           <div class="row">
+
+          <div class="col-md-12">
+            <button href="javascript.void(0)" class="btn btn-primary mb-3" data-target="#addBA" data-toggle="modal"><i class="far fa-plus-square"></i> Tambah data</button>
+          </div>
+
             <div class="col-12">
               <div class="card">
                 <!-- /.card-header -->
@@ -80,6 +66,49 @@ if (isset($_POST["status"])) {
 
                 </div>
               </div>
+
+              <!-- Modal Popup untuk Add-->
+            <div class="modal fade" id="addBA">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title">Tambah Data Berita Acara</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+
+                  <div class="modal-body">
+                    <form class="forms-sample" action="" method="post" enctype="multipart/form-data">
+                      <div class="form-group">
+                        <input type="hidden" class="form-control" name="id_ba" autocomplete="off" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="id_tha">Tanggal LHA<span style="color: red;">*</span></label>
+                        <select class="form-control " data-placeholder="Pilih Tanggal THA" style="width: 100%;" name="id_tha">
+                          <option value=""></option>
+                          <?php foreach ($tb_ba as $row) {
+                          ?>
+                            <option value="<?= $row['id_lha'] ?>"> <?php echo $row['tgl_lha']; ?> (ID LHA : <?= $row['id_lha']; ?>)</option>
+                          <?php } ?>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <label for="tgl_ba">Tanggal Berita Acara<span style="color: red;">*</span></label>
+                        <input type="date" class="form-control" id="tgl_ba" name="tgl_ba" autocomplete="off" required>
+                      </div>
+
+                      <button type="submit" class="btn btn-success mr-2 float-right" name="tambahLHA">Simpan</button>
+                      <button class="btn btn-secondary mr-2 float-right" data-dismiss="modal">Batal</button>
+                    </form>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <!-- akhir modal add -->
+
             </div>
           </div>
         </div>
